0648ec2dce64ede0184e4651f32eb1cae2ceea72
Hannes Kuchelmeister <hannes@kuchelmeister.org>
2022-04-18 12:02:33 +0200

    implement factory for toolbox_apps

--- src/ui/components/toolbox_apps/model.rs
+++ src/ui/components/toolbox_apps/model.rs
@@ -1,10 +1,17 @@
-use relm4::{Model};
+use relm4::{Model, factory::{FactoryVec}};
 
 
-use super::{messages::ToolboxAppDialogMsg, widgets::ToolboxAppDialogWidgets};
+use super::{messages::ToolboxAppDialogMsg, widgets::{ToolboxAppDialogWidgets}};
 
+#[derive(Default)]
+pub struct DotDesktopApplication {
+    pub name: String,
+    pub selected: bool,
+    pub icon_path: String,
+}
 pub struct ToolboxAppDialogModel {
     pub hidden: bool,
+    pub apps: FactoryVec<DotDesktopApplication>,
 }
 
 impl Model for ToolboxAppDialogModel {
